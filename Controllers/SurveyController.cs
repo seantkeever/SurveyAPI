09a31451a80051f7c338d02c1dd4e3173f85ca88
Sean Keever <sean@ravenlogistics.com>
2020-07-17 16:20:06 -0400

    Added more methods for insert and delete of Questions and Answers

--- Controllers/SurveyController.cs
+++ Controllers/SurveyController.cs
@@ -13,6 +13,9 @@ namespace SurveyAPI.Controllers
     [ApiController]
     public class SurveyController : ControllerBase
     {
+        //-----------------------------------------
+        //-------- Question Table Methods ----------
+        //-----------------------------------------
 
         [HttpPost("InsertQuestion")]
         public ActionResult<string> InsertQuestion([FromBody] System.Text.Json.JsonElement inputData)
@@ -58,12 +61,143 @@ namespace SurveyAPI.Controllers
                     return StatusCode(StatusCodes.Status500InternalServerError, x.Message);
                 }
             }
-
-
         }
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+
+
+        [HttpPost("DeleteQuestion")]
+        public ActionResult<string> DeleteQuestion([FromBody] System.Text.Json.JsonElement inputData)
+        {
+            short QuestionID = 0;
+            try
+            {
+                QuestionID = inputData.GetProperty("QuestionID").GetInt16();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, $"Survey DeleteQuestion - Invalid parameters detected");
+            }
+
+            DataAccess.SurveyAccess dataAccess = new DataAccess.SurveyAccess();
+            try
+            {
+                bool bResult = dataAccess.DeleteQuestion(QuestionID);
+                if (dataAccess._lastSqlException != null && !string.IsNullOrWhiteSpace(dataAccess._lastSqlException.Message))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, dataAccess._lastSqlException.Message);
+                }
+                else
+                {
+                    //Build output
+                    Output output = new Output(bResult, null);
+                    return Ok(JsonConvert.SerializeObject(output));
+                }
+            }
+            catch (Exception x)
+            {
+                if (dataAccess._lastSqlException != null && !string.IsNullOrWhiteSpace(dataAccess._lastSqlException.Message))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, dataAccess._lastSqlException.Message);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, x.Message);
+                }
+            }
+        }
+
+
+
+
+
+        //-----------------------------------------
+        //-------- Answer Table Methods ----------
+        //-----------------------------------------
+
+        [HttpPost("InsertAnswer")]
+        public ActionResult<string> InsertAnswer([FromBody] System.Text.Json.JsonElement inputData)
+        {
+            short QuestionID = 0;
+            bool? YesNo = false;
+            string Description = string.Empty;
+            try
+            {
+                QuestionID = inputData.GetProperty("QuestionID").GetInt16();
+                YesNo = inputData.GetProperty("YesNo").GetBoolean();
+                Description = inputData.GetProperty("Description").GetString();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, $"Survey InsertAnswer - Invalid parameters detected");
+            }
+
+            DataAccess.SurveyAccess dataAccess = new DataAccess.SurveyAccess();
+            try
+            {
+                bool bResult = dataAccess.InsertAnswer(QuestionID, YesNo, Description);
+                if (dataAccess._lastSqlException != null && !string.IsNullOrWhiteSpace(dataAccess._lastSqlException.Message))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, dataAccess._lastSqlException.Message);
+                }
+                else
+                {
+                    //Build output
+                    Output output = new Output(bResult, null);
+                    return Ok(JsonConvert.SerializeObject(output));
+                }
+            }
+            catch (Exception x)
+            {
+                if (dataAccess._lastSqlException != null && !string.IsNullOrWhiteSpace(dataAccess._lastSqlException.Message))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, dataAccess._lastSqlException.Message);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, x.Message);
+                }
+            }
+        }
+
+
+        [HttpPost("DeleteAnswer")]
+        public ActionResult<string> DeleteAnswer([FromBody] System.Text.Json.JsonElement inputData)
+        {
+            short AnswerID = 0;
+            try
+            {
+                AnswerID = inputData.GetProperty("AnswerID").GetInt16();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, $"Survey DeleteAnswer - Invalid parameters detected");
+            }
+
+            DataAccess.SurveyAccess dataAccess = new DataAccess.SurveyAccess();
+            try
+            {
+                bool bResult = dataAccess.DeleteAnswer(AnswerID);
+                if (dataAccess._lastSqlException != null && !string.IsNullOrWhiteSpace(dataAccess._lastSqlException.Message))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, dataAccess._lastSqlException.Message);
+                }
+                else
+                {
+                    //Build output
+                    Output output = new Output(bResult, null);
+                    return Ok(JsonConvert.SerializeObject(output));
+                }
+            }
+            catch (Exception x)
+            {
+                if (dataAccess._lastSqlException != null && !string.IsNullOrWhiteSpace(dataAccess._lastSqlException.Message))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, dataAccess._lastSqlException.Message);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, x.Message);
+                }
+            }
+        }
     }
 }
