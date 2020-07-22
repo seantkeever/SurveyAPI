425d89e5263ccc6e33a9f3efd875f43a1a0806e0
Sean Keever <sean@ravenlogistics.com>
2020-07-22 09:12:19 -0400

    Added Edit method for Question table

--- Controllers/SurveyController.cs
+++ Controllers/SurveyController.cs
@@ -105,6 +105,52 @@ namespace SurveyAPI.Controllers
             }
         }
 
+        [HttpPost("UpdateQuestion")]
+        public ActionResult<string> UpdateQuestion([FromBody] System.Text.Json.JsonElement inputData)
+        {
+            short QuestionID = 0;
+            string Description = string.Empty;
+            bool AllowYesNo = false;
+            bool AllowShortAnswer = false;
+            try
+            {
+                QuestionID = inputData.GetProperty("QuestionID").GetInt16();
+                Description = inputData.GetProperty("Description").GetString();
+                AllowYesNo = inputData.GetProperty("AllowYesNo").GetBoolean();
+                AllowShortAnswer = inputData.GetProperty("AllowShortAnswer").GetBoolean();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, $"Survey InsertQuestion - Invalid parameters detected");
+            }
+
+            DataAccess.SurveyAccess dataAccess = new DataAccess.SurveyAccess();
+            try
+            {
+                bool bResult = dataAccess.UpdateQuestion(QuestionID,Description, AllowYesNo, AllowShortAnswer);
+                if (dataAccess._lastSqlException != null && !string.IsNullOrWhiteSpace(dataAccess._lastSqlException.Message))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, dataAccess._lastSqlException.Message);
+                }
+                else
+                {
+                    //Build output
+                    Output output = new Output(bResult, null);
+                    return Ok(JsonConvert.SerializeObject(output));
+                }
+            }
+            catch (Exception x)
+            {
+                if (dataAccess._lastSqlException != null && !string.IsNullOrWhiteSpace(dataAccess._lastSqlException.Message))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, dataAccess._lastSqlException.Message);
+                }
+                else
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, x.Message);
+                }
+            }
+        }
 
 
 
